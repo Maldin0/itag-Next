8bd3eb44bbe3389266040bd6cc63c44a33d3efb1
65070014 <bosspopza150@hotmail.com>
2023-10-01 00:47:02 +0700

    update page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,31 @@
+"use client"
 import React from 'react';
 import Link from 'next/link'
+import axios from "axios";
 import profileStyle from './profileStyle.module.css'  ;
 import line from './images/line.png';
 import {log} from "util";
 
 type Props = {}
 
-export default async function Profile({ }: Props) {
-    async function Profile() {}
+
+export default function Profile({ }: Props) {
+    
+    // const [user, setUser] = React.useState<User | null>(null);
+    // const [email, setEmail] = React.useState<User | null>(null);
+    // React.useEffect(() => {
+    //     async function fetchUser() {
+    //       try {
+    //         const res = await axios.get("http://localhost:8080/users");
+    //         setUser(res.data);
+    //         setEmail(res.data);
+    //       } catch (err) {
+    //         console.error(err);
+    //       }
+    //     }
+    //     fetchUser();
+    //   }, []);
+      
     return (
         <div className={profileStyle.myImages}>
             <div className={profileStyle.scroll}>
@@ -17,11 +35,9 @@ export default async function Profile({ }: Props) {
                         style={{position: 'absolute', top: '20%', left: '3%', color: 'White'}}>ITAG</h1>
                 </div>
 
-                <div className={profileStyle.profile} style={{display:'flex', flexDirection:'column'}}>
-                    <div className={profileStyle.circle}>
-                    </div>
-                    <div className={profileStyle.username}>Username :</div>
-                    <div className={profileStyle.email}>Email :</div>
+                <div className={profileStyle.profile} style={{display:'inline-flex'}}>
+                    <div className={profileStyle.username}style={{display:'inline-block'}}>Username : </div>
+                    <div className={profileStyle.email} style={{display:'inline-block'}}>Email :</div>
                 </div>
 
                 <div className={profileStyle.line} >
