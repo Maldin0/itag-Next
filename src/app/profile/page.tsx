b1714e9ffd7a6254a400c0b0415262f1b9ee1f17
65070014 <bosspopza150@hotmail.com>
2023-10-01 07:46:23 +0700

    update profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import axios from "axios";
 import profileStyle from "./profileStyle.module.css";
@@ -13,6 +14,12 @@ import {
   ModalFooter,
   Button,
   useDisclosure,
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell,
 } from "@nextui-org/react";
 
 type Props = {};
@@ -52,6 +59,7 @@ interface Spell {
   interval_time: number;
   duration: number;
   range: number;
+  details: string;
 }
 
 interface Race {
@@ -94,8 +102,61 @@ export interface User {
 }
 
 export default function Profile({}: Props) {
+  const router = useRouter();
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [item_id, setItem_id] = React.useState<Number>(0);
+  const [char_id, setChar_id] = React.useState<Number>(0);
   const [user, setUser] = React.useState<User | null>(null);
-  const [modalOpen, setModalOpen] = useState<Record<number, boolean>>({});
+  const [openedModalDetails, setOpenedModalDetails] = useState<Number | null>(
+    null
+  );
+  const [openedModalInventory, setOpenedModalInventory] =
+    useState<Number | null>(null);
+
+  const allitems = {
+    "Health Potion": 1,
+    Antidote: 2,
+    Axe: 3,
+    Shield: 4,
+    Sword: 5,
+    Guitar: 6,
+    Bow: 7,
+    Staff: 8,
+    Grimoire: 9,
+    Dagger: 10,
+  };
+
+  async function handleRemove() {
+    try {
+      setLoading(true);
+      const res = await axios.post(
+        "http://localhost:8080/users/characters/remove_item",
+        {char_id,item_id}
+      );
+      if (res.data.status === "Success") {
+        alert(res.data.message);
+        router.refresh();
+      } else {
+        alert(res.data.message);
+        router.refresh();
+      }
+    } catch (error) {
+      // TODO: Still Noti the massage bitches
+      if (
+        axios.isAxiosError(error) &&
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        alert(error.response.data.message);
+      } else {
+        alert("An unexpected error occurred.");
+      }
+      router.refresh();
+    } finally {
+      setLoading(false);
+    }
+  }
 
   React.useEffect(() => {
     async function fetchUser() {
@@ -154,7 +215,7 @@ export default function Profile({}: Props) {
         <div
           className={profileStyle.char}
           style={{
-            paddingLeft: "350px",
+            paddingLeft: "300px",
             fontSize: "1.3rem",
             display: "inline-block",
           }}
@@ -174,7 +235,7 @@ export default function Profile({}: Props) {
         <div
           className={profileStyle.char}
           style={{
-            paddingLeft: "90px",
+            paddingLeft: "60px",
             fontSize: "1.3rem",
             display: "inline-block",
           }}
@@ -184,7 +245,7 @@ export default function Profile({}: Props) {
         <div
           className={profileStyle.char}
           style={{
-            paddingLeft: "100px",
+            paddingLeft: "80px",
             fontSize: "1.3rem",
             display: "inline-block",
           }}
@@ -194,17 +255,18 @@ export default function Profile({}: Props) {
         <div
           className={profileStyle.char}
           style={{
-            paddingLeft: "70px",
+            paddingLeft: "110px",
             fontSize: "1.3rem",
             display: "inline-block",
           }}
         >
           Inventory
         </div>
+
         <div
           className={profileStyle.char}
           style={{
-            paddingLeft: "70px",
+            paddingLeft: "80px",
             fontSize: "1.3rem",
             display: "inline-block",
           }}
@@ -229,45 +291,209 @@ export default function Profile({}: Props) {
             </div>
             <div>
               <Button
+                className={profileStyle.Clickme}
                 style={{ whiteSpace: "nowrap" }}
-                onPress={() =>
-                  setModalOpen((prev) => ({
-                    ...prev,
-                    [character._char_id]: true,
-                  }))
-                }
+                onPress={() => setOpenedModalDetails(character._char_id)}
               >
                 Click here
               </Button>
               <Modal
                 className={profileStyle.yourModalClass}
-                isOpen={modalOpen[character._char_id]}
-                onOpenChange={() =>
-                  setModalOpen((prev) => ({
-                    ...prev,
-                    [character._char_id]: !prev[character._char_id],
-                  }))
-                }
+                isOpen={openedModalDetails === character._char_id}
+                onOpenChange={() => setOpenedModalDetails(null)}
               >
                 <ModalContent>
                   {(onClose) => (
                     <>
+                      <ModalHeader>
+                        {" "}
+                        <h1
+                          className={profileStyle.font}
+                          style={{
+                            paddingTop: "20px",
+                            fontSize: "2rem",
+                            paddingLeft: "45%",
+                          }}
+                        >
+                          <strong>DETAILS</strong>
+                        </h1>
+                      </ModalHeader>
                       <ModalBody>
                         <div className={profileStyle.modaldetails}>
-                          <p><strong>Character name:</strong> {character.name}</p>
-                          <p><strong>Class name:</strong> {character.class.name}</p>
-                          <p><strong>Race name:</strong> {character.race.name}</p>
-                          <p><strong>Status:</strong></p>
-                          <p></p>
+                          <p>
+                            <strong>Character name:</strong> {character.name}
+                          </p>
+                          <p>
+                            <strong>Class name:</strong> {character.class.name}
+                          </p>
+                          <p>
+                            <strong>Race name:</strong> {character.race.name}
+                          </p>
+                          <p
+                            style={{
+                              paddingTop: "15px",
+                              textDecoration: "underline",
+                            }}
+                          >
+                            <strong>Status:</strong>
+                          </p>
+
                           {Object.entries(character.status).map(
                             ([key, value]) => (
-                              <div style={{paddingLeft:'15px'}} key={key}>
+                              <div
+                                style={{
+                                  paddingLeft: "15px",
+                                  paddingTop: "15px",
+                                }}
+                                key={key}
+                              >
                                 <strong>
                                   {key.charAt(0).toUpperCase() + key.slice(1)}:
                                 </strong>{" "}
                                 {value}
                               </div>
                             )
+                          )}
+                          <p
+                            style={{
+                              paddingTop: "15px",
+                              textDecoration: "underline",
+                            }}
+                          >
+                            <strong>BackStory:</strong>{" "}
+                          </p>
+                          <p>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            {character.background}
+                          </p>
+
+                          <p
+                            style={{
+                              paddingTop: "15px",
+                              textDecoration: "underline",
+                            }}
+                          >
+                            <strong>Skills:</strong>
+                          </p>
+
+                          {character.skills && character.skills.length > 0 ? (
+                            character.skills.map((skill, index) => (
+                              <div
+                                key={index}
+                                style={{
+                                  paddingLeft: "15px",
+                                  paddingTop: "15px",
+                                }}
+                              >
+                                <strong>{skill.name}:</strong>
+                                {skill.details && <span> {skill.details}</span>}
+                              </div>
+                            ))
+                          ) : (
+                            <p>
+                              &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;This character has
+                              no skill
+                            </p>
+                          )}
+
+                          <p
+                            style={{
+                              paddingTop: "15px",
+                              textDecoration: "underline",
+                            }}
+                          >
+                            <strong>Features:</strong>
+                          </p>
+                          {character.class.features &&
+                          character.class.features.length > 0 ? (
+                            character.class.features.map((feature, index) => (
+                              <div
+                                key={index}
+                                style={{
+                                  paddingTop: "15px",
+                                  paddingLeft: "15px",
+                                }}
+                              >
+                                <h3>
+                                  <strong>{feature.name}</strong>
+                                </h3>
+                                <p>&nbsp;&nbsp;&nbsp;&nbsp;{feature.detail}</p>
+                              </div>
+                            ))
+                          ) : (
+                            <p style={{ paddingLeft: "15px" }}>
+                              No features available for this class.
+                            </p>
+                          )}
+
+                          <p
+                            style={{
+                              paddingTop: "15px",
+                              textDecoration: "underline",
+                            }}
+                          >
+                            <strong>Spells:</strong>
+                          </p>
+                          {character.class.spell &&
+                          character.class.spell.length > 0 ? (
+                            character.class.spell.map((spell, index) => (
+                              <div
+                                key={index}
+                                style={{
+                                  paddingTop: "15px",
+                                  paddingLeft: "15px",
+                                }}
+                              >
+                                <h3>
+                                  <strong>{spell.name}</strong>
+                                </h3>
+                                <p style={{ paddingLeft: "10px" }}>
+                                  <strong>Interval Time:</strong>{" "}
+                                  {spell.interval_time}
+                                </p>
+                                <p style={{ paddingLeft: "10px" }}>
+                                  <strong>Duration:</strong> {spell.duration}
+                                </p>
+                                <p style={{ paddingLeft: "10px" }}>
+                                  <strong>Range:</strong> {spell.range}
+                                </p>
+                                <p style={{ paddingLeft: "10px" }}>
+                                  <strong>Detail:</strong> {spell.details}
+                                </p>
+                              </div>
+                            ))
+                          ) : (
+                            <p style={{ paddingLeft: "15px" }}>
+                              No spells available for this class.
+                            </p>
+                          )}
+
+                          <p
+                            style={{
+                              paddingTop: "15px",
+                              textDecoration: "underline",
+                            }}
+                          >
+                            <strong>Traits:</strong>
+                          </p>
+                          {character.race.traits &&
+                          character.race.traits.length > 0 ? (
+                            character.race.traits.map((trait, index) => (
+                              <div
+                                key={index}
+                                style={{
+                                  paddingTop: "15px",
+                                  paddingLeft: "15px",
+                                }}
+                              >
+                                <h3>
+                                  <strong>{trait.name}</strong>
+                                </h3>
+                                <p>&nbsp;&nbsp;&nbsp;{trait.detail}</p>
+                              </div>
+                            ))
+                          ) : (
+                            <p>No traits available for this race.</p>
                           )}
                         </div>
                       </ModalBody>
@@ -277,8 +503,96 @@ export default function Profile({}: Props) {
                 </ModalContent>
               </Modal>
             </div>
+            <div>
+              <Button
+                className={profileStyle.Clickme}
+                style={{ whiteSpace: "nowrap" }}
+                onPress={() => setOpenedModalInventory(character._char_id)}
+              >
+                Click here
+              </Button>
+              <Modal
+                className={profileStyle.yourModalClass}
+                isOpen={openedModalInventory === character._char_id}
+                onOpenChange={() => setOpenedModalInventory(null)}
+              >
+                <ModalContent>
+                  {(onClose) => (
+                    <>
+                      <ModalHeader>
+                        {" "}
+                        <h1
+                          className={profileStyle.font}
+                          style={{
+                            paddingTop: "20px",
+                            fontSize: "2rem",
+                            paddingLeft: "45%",
+                          }}
+                        >
+                          <strong>INVENTORY</strong>
+                        </h1>
+                      </ModalHeader>
+                      <ModalBody>
+                        <p>&nbsp;</p>
+                        <Table>
+                          <TableHeader>
+                            <TableColumn>ITEM</TableColumn>
+                            <TableColumn>DETAIL</TableColumn>
+                            <TableColumn>STATUS</TableColumn>
+                          </TableHeader>
+                          <TableBody>
+                            {user?.user._char[_char_id]?.bag.map(
+                              (item, index) => (
+                                <TableRow
+                                  key={index}
+                                  className={profileStyle.modaltable}
+                                >
+                                  <TableCell style={{ paddingTop: "30px" }}>
+                                    {item.name}
+                                  </TableCell>
+                                  <TableCell
+                                    style={{
+                                      paddingTop: "30px",
+                                      paddingLeft: "250px",
+                                    }}
+                                  >
+                                    {item.detail}
+                                  </TableCell>
+                                  <TableCell style={{ paddingTop: "30px" }}>
+                                    <div
+                                      className={profileStyle.Clickme}
+                                      onClick={() => {
+                                        setItem_id(
+                                          allitems[
+                                            item.name as keyof typeof allitems
+                                          ]
+                                        );
+                                        setChar_id(character._char_id);
+                                        console.log(character._char_id);
+                                        console.log(allitems[
+                                          item.name as keyof typeof allitems
+                                        ]);
+                                        handleRemove();
+                                      }}
+                                    >
+                                      <button>Delete</button>
+                                    </div>
+                                  </TableCell>
+                                </TableRow>
+                              )
+                            )}
+                          </TableBody>
+                        </Table>
+                      </ModalBody>
+                      <ModalFooter></ModalFooter>
+                    </>
+                  )}
+                </ModalContent>
+              </Modal>
+            </div>
           </div>
         ))}
+        <div></div>
 
         <Link href="/character">
           <div
